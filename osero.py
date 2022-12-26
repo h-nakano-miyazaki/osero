f167e54e62fdcc012f903c7905e2bdc90dc1a077
h-nakano <nakano.hiroaki@miyazaki-nw.or.jp>
2022-12-26 13:04:35 +0900

    AI実装

--- osero.py
+++ osero.py
@@ -1,33 +1,39 @@
 import tkinter
+import tkinter.messagebox
+import random
+
+FS=("Times New Roman",30)
+FL=("Times New Roman",80)
+
 BLACK=1
 WHITE=2
 
-board=[
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,2,1,0,0,0],
-[0,0,0,1,2,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0],
-]
+mx=0
+my=0
+mc=0
+proc=0
+turn=0
+msg=""
+space=0
+color=[0]*2
+who=["あなた","コンピュータ"]
+
+board=[]
+for y in range(8):
+    board.append([0,0,0,0,0,0,0,0])
 
 def click(e):
+    global mx,my,mc
+    mc=1
     mx=int(e.x/80)
     my=int(e.y/80)
     if mx>7:mx=7
     if my>7:my=7
-    if board[my][mx]==0:
-        board[my][mx]=BLACK
-    elif board[my][mx]==BLACK:
-        board[my][mx]=WHITE
-    elif board[my][mx]==WHITE:
-        board[my][mx]=0
-    banmen()
+
 
 def banmen():
     cvs.delete("all")
+    cvs.create_text(320,670,text=msg,fill="silver",font=FS)
     for y in range(8):
         for x in range(8):
             X=x*80
@@ -37,6 +43,21 @@ def banmen():
                 cvs.create_oval(X+10,Y+10,X+70,Y+70,fill="black",width=0)
             if board[y][x]==WHITE:
                 cvs.create_oval(X+10,Y+10,X+70,Y+70,fill="white",width=0)
+#置ける場所
+#            if kaeseru(x,y,BLACK)>0:
+#                cvs.create_oval(X+5,Y+5,X+75,Y+75,outline="cyan",width=2)
+    cvs.update()
+
+def ban_shokika():
+    global space
+    space=60
+    for y in range(8):
+        for x in range(8):
+            board[y][x]=0
+    board[3][4]=BLACK
+    board[4][3]=BLACK
+    board[3][3]=WHITE
+    board[4][4]=WHITE
 
 #石を打ちひっくり返す
 def ishi_utsu(x,y,iro):
@@ -44,7 +65,8 @@ def ishi_utsu(x,y,iro):
     for dy in range(-1,2):
         for dx in range(-1,2):
             k=0
-            sx=xsy=y
+            sx=x
+            sy=y
             while True:
                 sx+=dx
                 sy+=dy
@@ -52,12 +74,149 @@ def ishi_utsu(x,y,iro):
                 if sx<0 or sx>7 or sy<0 or sy>7:
                     break
                 #何も置かれていないマスなら
+                if board[sy][sx]==0:
+                    break
+                #相手の石があれば
+                if board[sy][sx]==3-iro:
+                    k+=1
+                #自分の石があれば
+                if board[sy][sx]==iro:
+                    for i in range(k):
+                        sx-=dx
+                        sy-=dy
+                        board[sy][sx]=iro
+                    break
+
+#そこに打つといくつ返せるか数える
+def kaeseru(x,y,iro):
+    if board[y][x]>0:
+        return -1
+    total=0
+    for dy in range(-1,2):
+        for dx in range(-1,2):
+            k=0
+            sx=x
+            sy=y
+            while True:
+                sx+=dx
+                sy+=dy
+                if sx<0 or sx>7 or sy<0 or sy>7:
+                    break
+                if board[sy][sx]==0:
+                    break
+                if board[sy][sx]==3-iro:
+                    k+=1
+                if board[sy][sx]==iro:
+                    total+=k
+                    break
+    return total
+
+#打てる石があるか調べる
+def uteru_masu(iro):
+    for y in range(8):
+        for x in range(8):
+            if kaeseru(x,y,iro)>0:
+                return True
+    return False
+
+#黒い石、白い石、いくつあるか数える
+def ishino_kazu():
+    b=0
+    w=0
+    for y in range(8):
+        for x in range(8):
+            if board[y][x]==BLACK:
+                b+=1
+            if board[y][x]==WHITE:
+                w+=1
+    return b,w
+
+#コンピュータの思考ルーチン
+def computer_0(iro):
+    while True:
+        rx=random.randint(0,7)
+        ry=random.randint(0,7)
+        if kaeseru(rx,ry,iro)>0:
+            return rx,ry
+
+def main():
+    global mc,proc,turn,msg,space
+    banmen()
+
+    if proc==0:
+        msg="先手、後手を選んでください"
+        cvs.create_text(320,200,text="オセロ",fill="gold",font=FL)
+        cvs.create_text(160,440,text="先手（黒）",fill="lime",font=FS)
+        cvs.create_text(480,440,text="後手（白）",fill="lime",font=FS)
+        if mc==1:
+            mc=0
+            if(mx==1 or mx==2) and my==5:
+                ban_shokika()
+                color[0]=BLACK
+                color[1]=WHITE
+                turn=0
+                proc=1
+            if(mx==5 or mx==6) and my==5:
+                ban_shokika()
+                color[0]=WHITE
+                color[1]=BLACK
+                turn=1
+                proc=1
+
+    elif proc==1:
+        msg="あなたの番です"
+        if turn==1:
+            msg="コンピュータ　考え中"
+        proc=2
+    elif proc==2:
+        if turn==0:
+            if mc==1:
+                mc=0
+                if kaeseru(mx,my,color[turn])>0:
+                    ishi_utsu(mx,my,color[turn])
+                    space-=1
+                    proc=3
+        else:
+            cx,cy=computer_0(color[turn])
+            ishi_utsu(cx,cy,color[turn])
+            space-=1
+            proc=3
+    elif proc==3:
+        msg=""
+        turn=1-turn
+        proc=4
+    elif proc==4:
+        if space==0:
+            proc=5
+        elif uteru_masu(BLACK)==False and uteru_masu(WHITE)==False:
+            tkinter.messagebox.showinfo("","どちらも打てないので終了です")
+            proc=5
+        elif uteru_masu(color[turn])==False:
+            tkinter.messagebox.showinfo("",who[turn]+"は打てないのでパス")
+            proc=3
+        elif turn==1 and uteru_masu(WHITE)==False:
+            msg="コンピュータが打てないのでパス"
+            proc=3
+        else:
+            proc=1
+    elif proc==5:
+        b,w=ishino_kazu()
+        msg = "黒=" + str(b) +"、白=" + str(w)
+        tkinter.messagebox.showinfo("終了",msg)
+        if (color[0]==BLACK and b>w) or (color[0]==WHITE and w>b):
+            tkinter.messagebox.showinfo("","あなたの勝ち！")
+        elif (color[1]==BLACK and b>w) or (color[1]==WHITE and w>b):
+            tkinter.messagebox.showinfo("","コンピュータの勝ち！")
+        else:
+            tkinter.messagebox.showinfo("","引き分け")
+        proc=0
+    root.after(100,main)
 
 root=tkinter.Tk()
-root.title("五目並べ")
+root.title("オセロ")
 root.resizable(False,False)
 root.bind("<Button>",click)
 cvs=tkinter.Canvas(width=640,height=700,bg="green")
 cvs.pack()
-banmen()
+root.after(100,main)
 root.mainloop()
